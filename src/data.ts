cba25baedcbf532a8a1c71e06dcabc6e806d45f1
justina177 <justinaenenche@yahoo.com>
2023-08-20 08:29:53 +0100

    map data in topBox component and implement in homepage

--- src/data.ts
+++ src/data.ts
@@ -114,3 +114,56 @@ export const menu = [
       ],
     },
   ];
+
+
+  export const topDealUsers = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      username: "Elizabeth Morgan",
+      email: "elimorgan@gmail.com",
+      amount: "4.668",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      username: "Karren Clark",
+      email: "karren@gmail.com",
+      amount: "3.256",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      username: "Brent Roman",
+      email: "brent@gmail.com",
+      amount: "2.998",
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      username: "Adeline ethan",
+      email: "adeline@gmail.com",
+      amount: "2.512",
+    },
+    {
+      id: 5,
+      img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      username: "Juan Harrington",
+      email: "juan@gmail.com",
+      amount: "2.134",
+    },
+    {
+      id: 6,
+      img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      username: "Augusta McGee",
+      email: "augusta@gmail.com",
+      amount: "1.932",
+    },
+    {
+      id: 7,
+      img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      username: "Angel Blake",
+      email: "angel@gmail.com",
+      amount: "1.560",
+    },
+  ];
