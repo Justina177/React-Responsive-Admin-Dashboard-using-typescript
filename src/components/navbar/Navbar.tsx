b835e9be9b49b69b9bb69872b40ebfe09f34661d
justina177 <justinaenenche@yahoo.com>
2023-08-20 04:46:28 +0100

    Added Navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,31 @@
-import './navbar.scss'
+import './navbar.scss';
+import logo from '../../img/j-logo.jpeg';
+import img1 from '../../img/pexels-pixabay-38554-removebg-preview.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="" className="logoImg" />
+        <span>Justadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src={img1} alt="" />
+          <span>Ruth</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
 
-export default Navbar
+    </div>
+  );
+};
+
+export default Navbar;
