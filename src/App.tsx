c03236a8e56bc55572393c40e4709c2c61ab580c
justina177 <justinaenenche@yahoo.com>
2023-08-20 02:55:40 +0100

    implemented routes for the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
 import Home from "./Pages/Home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Users from "./Pages/users/Users";
+import Products from "./Pages/products/Products";
+
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu/>
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+          <Footer />
+        </div>
+      </div>
+    )
+  };
+  
+  const router = createBrowserRouter([
+    {
+      path:"/", element:<Layout/>,
+      children:[
+        {
+          path:"/", element:<Home />
+        },
+        {
+          path:"/users", element:<Users />
+        },
+        {
+          path:"/products", element:<Products />
+        }
+        
 
+      ]
+      
+    
+
+    }
+      
+    ]);
+    
+    
   return (
-    <div>
-      <Home />
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
