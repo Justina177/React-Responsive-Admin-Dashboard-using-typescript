f564fc4de5a57706bbc7ba50840561fe10c596dc
justina177 <justinaenenche@yahoo.com>
2023-08-21 21:15:53 +0100

    Added PieChartBox Component

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from '../../components/barChartBox/BarChartBox';
 import ChartBox from '../../components/chartBox/ChartBox';
+import PieChartBox from '../../components/pieChartBox/PieChartBox';
 import TopBox from '../../components/topBox/TopBox';
 import { chartBoxUser, chartBoxProduct, chartBoxRevenue, chartBoxConversion, barChartBoxVisit, barChartBoxRevenue } from '../../data';
 import './home.scss';
@@ -10,7 +11,7 @@ const Home = () => {
       <div className="box box1"><TopBox /></div>
       <div className="box box2"><ChartBox {...chartBoxUser} /></div>
       <div className="box box3"><ChartBox {...chartBoxProduct} /></div>
-      <div className="box box4"></div>
+      <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox {...chartBoxConversion} /></div>
       <div className="box box6"><ChartBox {...chartBoxRevenue} /></div>
       <div className="box box7">Box7</div>
