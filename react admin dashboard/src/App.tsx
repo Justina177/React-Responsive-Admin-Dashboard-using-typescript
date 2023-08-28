e43530bd878fb9c3b298ced4bd7bd3738de28aa5
justina177 <justinaenenche@yahoo.com>
2023-08-28 18:58:00 +0100

    Added function to delete user

---
+++ react admin dashboard/src/App.tsx
@@ -0,0 +1,74 @@
+import Home from "./Pages/Home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Users from "./Pages/users/Users";
+import Products from "./Pages/products/Products";
+import Login from "./Pages/login/Login";
+import "./styles/global.scss"
+import User from "./Pages/user/User";
+import Product from "./Pages/product/Product";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu/>
+          </div>
+          <div className="contentContainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+            
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  };
+  
+  const router = createBrowserRouter([
+    {
+      path:"/", element:<Layout/>,
+      children:[
+        {
+          path:"/", element:<Home />
+        },
+        {
+          path:"/users", element:<Users />
+        },
+        {
+          path:"/products", element:<Products />
+        },
+        {
+          path:"/users/:id", element:<User />
+        },
+        {
+          path:"/products/:id", element:<Product />
+        },
+      ],
+    },
+    {
+      path:"/login", element:<Login />
+    }
+      
+    ]);
+    
+    
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App;
---
+++ react admin dashboard/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Justadmin</span>
+      <span>©Just Tech Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
